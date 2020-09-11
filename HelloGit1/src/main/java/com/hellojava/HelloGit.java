ae58e5685ae8163876f371bb0ef74648d2adee44
tom_glb <goodsMorning_glb@atguigu.com>
2020-09-11 21:14:33 +0800

    创建分支

--- HelloGit1/src/main/java/com/hellojava/HelloGit.java
+++ HelloGit1/src/main/java/com/hellojava/HelloGit.java
@@ -4,8 +4,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("这世界，我来了!");
         System.out.println("更新1");
-        System.out.println("更新2");
-        System.out.println("更新3");
-        System.out.println("更新4");
+        System.out.println("我是创建的分支!");
     }
 }
